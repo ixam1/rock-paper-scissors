212376bfea47f1b3181c099875c792da03c65f55
Maximilian Schaum <max.schaum4@gmail.com>
2022-04-19 12:41:23 +0200

    ✨ improved ipad spacing

--- src/components/stages/ConfigStage.tsx
+++ src/components/stages/ConfigStage.tsx
@@ -10,17 +10,17 @@ function ConfigStage({
 }) {
 	return (
 		<div className="flex flex-col items-center gap-8">
-			<div className="w-full p-6 shadow sm:p-8 bg-slate-50 sm:w-96 rounded-xl dark:bg-slate-800">
-				<h1 className="text-2xl font-extrabold tracking-wider uppercase sm:text-3xl">
+			<div className="w-full p-6 shadow xl:p-8 bg-slate-50 sm:w-96 rounded-xl dark:bg-slate-800">
+				<h1 className="text-2xl font-extrabold tracking-wider uppercase xl:text-3xl">
 					<div>Rock</div> <div>Paper</div> <div>Scissors</div>
 				</h1>
 			</div>
 
-			<fieldset className="w-full p-6 pt-6 border sm:p-8 border-slate-600 dark:border-slate-300 rounded-xl sm:w-96">
+			<fieldset className="w-full p-6 pt-6 border xl:p-8 border-slate-600 dark:border-slate-300 rounded-xl sm:w-96">
 				<legend className="px-2 font-light uppercase text-slate-600 dark:text-slate-300">
 					Play now
 				</legend>
-				<div className="space-y-6 sm:space-y-8">
+				<div className="space-y-6 xl:space-y-8">
 					{gameConfigs.map((config) => (
 						<PrimaryButton
 							className="w-full"
